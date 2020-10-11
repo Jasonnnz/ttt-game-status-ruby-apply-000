2b8084fa9b81dd6544ef2b7bdbb1233602e3dde3
bustling-procedure-0781 <jzhou35@buffalo.edu>
2020-10-11 02:45:20 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -29,7 +29,7 @@ def won?(board)
     position_3 = board[win_index_3]
     
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combination
+      return win_combination 
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combination
     else
@@ -64,7 +64,7 @@ def winner(board)
   if winning_array == false 
     return nil
   else
-    return 
+    return board[winning_array[0]] 
   end
 end
 
