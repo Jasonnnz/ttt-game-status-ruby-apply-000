6abf3383e450900d8092df2b4755db8011069772
bustling-procedure-0781 <jzhou35@buffalo.edu>
2020-10-11 00:53:23 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -52,7 +52,9 @@ def draw?(board)
 end
 
 def over?(board)
-  if won?(board) == true || draw?(board) == true
+  if won?(board) == true && full?(board) == false 
+    return true
+  elsif won?(board) == true || draw?(board) == true
     return true
   elsif full?(board) == true
     return true
